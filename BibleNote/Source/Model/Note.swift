e6ce4d1677bfea7ff398cc6d2dc36214c74496be
Chae Haram <djvemfla2@gmail.com>
2024-01-23 13:21:33 +0900

    add BibleNote Model

---
+++ BibleNote/Source/Model/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  BibleNote
+//
+//  Created by Chae_Haram on 1/23/24.
+//
+
+import Foundation
+
+struct Note {
+    let title: String
+    let mainVerse: String
+    let note: String
+    let date: Date
+}
