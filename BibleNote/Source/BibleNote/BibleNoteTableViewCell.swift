7752a38f498f728baa60f32657e95d08c0c05598
Chae Haram <djvemfla2@gmail.com>
2024-01-23 13:20:12 +0900

    add noteCell

---
+++ BibleNote/Source/BibleNote/BibleNoteTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BibleNoteTableViewCell.swift
+//  BibleNote
+//
+//  Created by Chae_Haram on 1/23/24.
+//
+
+import UIKit
+
+class BibleNoteTableViewCell: UITableViewCell {
+
+    static let identifier = "noteCell"
+
+    @IBOutlet weak var BibleNoteTitleLabel: UILabel!
+    
+}
